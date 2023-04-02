acec1e3f7abbd0f089a0e3d2f66a8d71fa7a9917
Cozark <longho250702@gmail.com>
2023-04-02 22:33:41 +0700

    Update datetime

--- src/main/java/teleder/core/services/User/dtos/UserDto.java
+++ src/main/java/teleder/core/services/User/dtos/UserDto.java
@@ -2,15 +2,16 @@ package teleder.core.services.User.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.joda.time.DateTime;
 
 import java.util.Date;
 
 @Data
 public class UserDto extends UpdateUserDto {
     @JsonProperty(value = "createAt")
-    public Date createAt;
+    public DateTime createAt;
     @JsonProperty(value = "updateAt")
-    public Date updateAt;
+    public DateTime updateAt;
     @JsonProperty(value = "id")
     private String id;
 
